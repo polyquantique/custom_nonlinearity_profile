8786278c6b9b0f2e3b01ec5c719cf2b938b32845
Nicolas Quesada <nquesada@pop-os.localdomain>
2022-07-04 11:39:35 -0400

    Passed black and linted :)

--- code/path_finder_lib.py
+++ code/path_finder_lib.py
@@ -1,10 +1,31 @@
+# Copyright 2022 Salvador Poveda-Hospital
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Basic library for designing waveguides with a given nonlinearity profile along the propagation direction
+"""
+
+# pylint: disable=C0103,C0301,R0913
+
 import numpy as np
 
-def find_path(f,N,delta=0.00002,d=1.0,d0=0.0,k=2,Pini=0.0): 
+
+def find_path(f, N, delta=0.00002, d=1.0, d0=0.0, k=2, Pini=0.0):
     """
     Function to find the path given a specified nonlinearity profile
-    
-    Args: 
+
+    Args:
         f(function): nonlinearity profile
         N(int): number of grid points
         delta(float): discretization in space
@@ -12,36 +33,36 @@ def find_path(f,N,delta=0.00002,d=1.0,d0=0.0,k=2,Pini=0.0):
         d0(float): angle independent susceptibility
         k(int): order angle dependency
         Pini(float): initial z position
-    
-    Returns: 
-        tuple(array, array, array, array):(x path, z path, effective susceptibility, propagation distance)  
+
+    Returns:
+        tuple(array, array, array, array):(x path, z path, effective susceptibility, propagation distance)
     """
-    x = np.zeros(N)      # \rho in the manuscript
-    s = np.zeros(N)      # arc length
+    x = np.zeros(N)  # \rho in the manuscript
+    s = np.zeros(N)  # arc length
     z = np.zeros(N)
     deff = np.zeros(N)
     theta = np.zeros(N)
-    
+
     # initial values
     z[0] = Pini
     z[1] = Pini
     s[0] = Pini
     s[1] = Pini
-    
-    for i in range(2, N):   
-        if -np.pi/4 < theta[i-1] < np.pi/4:
-            z[i] = z[i-1]+delta;
-            s[i] = s[i-1] + np.sqrt((x[i-1]-x[i-2])**2+(z[i]-z[i-1])**2);
-            deff[i]=f(s[i])
-            theta[i] = (1.0/k)*np.arcsin(d*f(s[i])-d0);
-            dx = np.tan(theta[i]);
-            x[i] = x[i-1] + delta*dx      
-        else:             
-            x[i] = x[i-1] + delta;
-            s[i] = s[i-1] + np.sqrt((x[i]-x[i-1])**2+(z[i-1]-z[i-2])**2);
-            deff[i]=f(s[i])
-            theta[i] = (1.0/k)*np.arcsin(d*f(s[i])-d0);
-            dz = np.tan(theta[i]);
-            z[i] = z[i-1] + np.sign(theta[i])*delta/dz  
-            x[i] = x[i-1] + np.sign(theta[i])*delta;
-    return x,z,deff,s
+
+    for i in range(2, N):
+        if -np.pi / 4 < theta[i - 1] < np.pi / 4:
+            z[i] = z[i - 1] + delta
+            s[i] = s[i - 1] + np.sqrt((x[i - 1] - x[i - 2]) ** 2 + (z[i] - z[i - 1]) ** 2)
+            deff[i] = f(s[i])
+            theta[i] = (1.0 / k) * np.arcsin(d * f(s[i]) - d0)
+            dx = np.tan(theta[i])
+            x[i] = x[i - 1] + delta * dx
+        else:
+            x[i] = x[i - 1] + delta
+            s[i] = s[i - 1] + np.sqrt((x[i] - x[i - 1]) ** 2 + (z[i - 1] - z[i - 2]) ** 2)
+            deff[i] = f(s[i])
+            theta[i] = (1.0 / k) * np.arcsin(d * f(s[i]) - d0)
+            dz = np.tan(theta[i])
+            z[i] = z[i - 1] + np.sign(theta[i]) * delta / dz
+            x[i] = x[i - 1] + np.sign(theta[i]) * delta
+    return x, z, deff, s
--- code/test_path_finder_lib.py
+++ code/test_path_finder_lib.py
@@ -1,25 +1,65 @@
+# Copyright 2022 Salvador Poveda-Hospital
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Basic library for designing waveguides with a given nonlinearity profile along the propagation direction
+"""
+
+# pylint: disable=C0103,C0301,R0913
+
+import pytest
 import numpy as np
-import matplotlib.pyplot as plt
+from path_finder_lib import find_path
 
-def analytical(x,x0,y0,b,radius):
+
+def analytical(x, x0, y0, b, radius):
+    """Gives the analytical solution for a sine nonlinearity with k=1"""
     if b:
-        return  -np.sqrt(radius**2-(x-x0)**2)+y0
-    else:
-        return   np.sqrt(radius**2-(x-x0)**2)+y0
+        return -np.sqrt(radius**2 - (x - x0) ** 2) + y0
+    return np.sqrt(radius**2 - (x - x0) ** 2) + y0
 
-def test_sine(z, N=30000, delta=0.00002, Lambda=0.1, k=1):
-    radius=Lambda*k/2/np.pi
-    z_test = np.zeros(N)    
-    x_test = np.zeros(N) 
-    z_test[0]=0
-    x_test[0]=0
+
+def solution_sine(z, N=30000, Lambda=0.1, k=1):
+    """Gives the path for the analytical solution"""
+    radius = Lambda * k / 2 / np.pi
+    z_test = np.zeros(N)
+    x_test = np.zeros(N)
+    z_test[0] = 0
+    x_test[0] = 0
 
     for i in range(1, N):
-        z_test[i] = z[i-1] 
-        if np.mod((z_test[i]-radius),4*radius) < 2*radius:
-            b=0
+        z_test[i] = z[i - 1]
+        if np.mod((z_test[i] - radius), 4 * radius) < 2 * radius:
+            b = 0
         else:
-            b=1
-        x_test[i]=analytical(z_test[i],int((z_test[i]+radius)/(2*radius))*2*radius,radius,b,radius)
+            b = 1
+        x_test[i] = analytical(
+            z_test[i],
+            int((z_test[i] + radius) / (2 * radius)) * 2 * radius,
+            radius,
+            b,
+            radius,
+        )
 
-    return z_test, x_test
+    return x_test
+
+
+@pytest.mark.parametrize("Lambda", [0.1, 0.2, 0.3])
+def test_circles(Lambda):
+    """Test that the solution from the path finder is correct for a sine nonlinearity"""
+    d_sin_local = lambda s: np.sin(2 * np.pi * s / Lambda)
+    k = 1
+    x, z, _, _ = find_path(d_sin_local, 50000, delta=0.00001, k=k)
+    x_test = solution_sine(z, N=50000, Lambda=Lambda, k=1)
+    assert np.allclose(x, x_test, atol=0.05)
